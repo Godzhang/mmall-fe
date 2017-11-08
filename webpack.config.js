5ec95b4629bf3aa6b10fd43f86cfdfa7708cd0b7
godzhang <zhangqijilindaxue@126.com>
2017-11-08 23:04:44 +0800

    initial

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 /*
 * @Author: admin
 * @Date:   2017-11-07 21:14:31
- * @Last Modified by: zhangqi
- * @Last Modified time: 2017-11-08 11:50:55
+ * @Last Modified by:   admin
+ * @Last Modified time: 2017-11-08 22:56:16
 */
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//环境变量配置，dev / online
+var WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
+console.log(WEBPACK_ENV)
+
 //获取HtmlWebpackPlugin参数的方法
 var getHtmlConfig = function(name){
     return {
@@ -21,7 +26,7 @@ var getHtmlConfig = function(name){
 
 var config = {
     entry: {
-        'common': ['./src/page/common/index.js', 'webpack-dev-server/client?http://localhost:8088/'],
+        'common': ['./src/page/common/index.js'],
         'index': ['./src/page/index/index.js'],
         'login': ['./src/page/login/index.js']
     },
@@ -52,5 +57,9 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('login'))
     ]
 };
+
+if('dev' === WEBPACK_ENV){
+    config.entry.common.push('webpack-dev-server/client?http://localhost:8088/')
+}
 
 module.exports = config;
