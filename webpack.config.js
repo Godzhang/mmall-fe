1362ab08972151b2224cfebc45b5f3535955ab24
godzhang <zhangqijilindaxue@126.com>
2017-11-08 14:49:56 +0800

    speed so bad

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,33 @@
 /*
 * @Author: admin
 * @Date:   2017-11-07 21:14:31
-* @Last Modified by:   admin
-* @Last Modified time: 2017-11-08 00:56:41
+ * @Last Modified by: zhangqi
+ * @Last Modified time: 2017-11-08 11:50:55
 */
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+//获取HtmlWebpackPlugin参数的方法
+var getHtmlConfig = function(name){
+    return {
+        template: './src/view/'+ name +'.html',
+        filename: 'view/'+ name +'.html',
+        inject: true,
+        hash: true,
+        chunks: ['common', name],
+        title: name
+    }
+}
 
 var config = {
     entry: {
-        'common': ['./src/page/common/index.js'],
+        'common': ['./src/page/common/index.js', 'webpack-dev-server/client?http://localhost:8088/'],
         'index': ['./src/page/index/index.js'],
         'login': ['./src/page/login/index.js']
     },
     output: {
         path: './dist',
+        publicPath: '/dist',
         filename: 'js/[name].js'
     },
     externals: {
@@ -22,15 +35,21 @@ var config = {
     },
     module: {
         loaders: [
-            {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader','css-loader')}
+            {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader','css-loader')},
+            {test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100&name=../resource/[name].[ext]'}
         ]
     },
     plugins: [
+        //独立通用模块到js/base.js
         new webpack.optimize.CommonsChunkPlugin({
             name: 'common',
             filename: 'js/base.js'
         }),
-        new ExtractTextPlugin('css/[name].css')
+        //把css单独打包到文件里
+        new ExtractTextPlugin('css/[name].css'),
+        //html模板的处理
+        new HtmlWebpackPlugin(getHtmlConfig('index')),
+        new HtmlWebpackPlugin(getHtmlConfig('login'))
     ]
 };
 
